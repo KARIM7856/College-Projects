7b6b3d381756278f43523f8fede9a0a5973b571e
KARIM7856 <36071198+KARIM7856@users.noreply.github.com>
2020-03-27 01:32:16 +0200

    Update mGraph.java

--- task1Project/src/mGraph.java
+++ task1Project/src/mGraph.java
@@ -8,21 +8,31 @@ public class mGraph {
     public int V;				//# Vertices
     public int E;			        // # Edges
     public LinkedList<Integer>[] adj;	// Adjacency List
+    public LinkedList<Integer>[] adj2;
     public int[][] adjMat; // adjacency matrix.
     public int[][ ] incMat; // incidence matrix.
     private int currentIncColum; // incidence matrix related variable.
+    private String euler;
 
-    mGraph(int vertex, int edges)
+    public String getEuler() {
+		return euler;
+	}
+	public void setEuler(String euler) {
+		this.euler = euler;
+	}
+	mGraph(int vertex, int edges)
     {
         V = vertex;
         E = edges;
         currentIncColum = 0;
         adj = new LinkedList[V];
+        adj2 = new LinkedList[V];
         adjMat = new int[V][V];
         incMat = new int[V][E];
         for (int i =0; i < V; i++)
         {
             adj[i] = new LinkedList<Integer>();          // Initializing Adjacency List
+            adj2[i] = new LinkedList<Integer>(); 
         }
 
         for (int j = 0; j < V; j++)
@@ -53,8 +63,10 @@ public class mGraph {
 
     void addEdge(int v1, int v2)
     {
-        adj[v1].add(v2);
-        adj[v2].add(v2);
+        adj[v2].add(v1);
+        adj2[v2].add(v1);
+        adj2[v1].add(v2);
+        
         adjMat[v1][v2] = 1;
         incMat[v1][currentIncColum] = 1;
         incMat[v2][currentIncColum++] = 1;   
@@ -83,8 +95,8 @@ public class mGraph {
     public int checkEuler() {
     	int counter = 0;
     	int result = 0;
-    	for(int i = 0; i < adj.length; i++) {
-    		if(adj[i].size() % 2 != 0) {
+    	for(int i = 0; i < adj2.length; i++) {
+    		if(adj2[i].size() % 2 != 0) {
     			System.out.println(adj[i].size());
     			counter++;
     			result = i;
@@ -105,13 +117,66 @@ public class mGraph {
     }
     
     public ArrayList<Integer> fleurys() {
+    	euler = "";
+    	
     	int condition = checkEuler();
-    	LinkedList <Integer>[] availEdges = adj;
+    	LinkedList <Integer>[] availEdges = adj2;
     	ArrayList<Integer> ePath = new ArrayList();
-    	int i = 0;
-    	int nextVertix = 0;
+    	Integer i = condition;
+    	Integer nextVertix = 0;
+    	
+    	int counter = 0;
     	if(condition == 0) {
-    		while(availEdges[i].get(0) != 0) {
+    		
+    		
+    		ePath.add(0);
+    		euler += 0;
+    		
+    		
+    		while(counter < E) {
+    			
+    			for(int j = 0; j < availEdges[i].size(); j++) {
+    				
+    				if(availEdges[availEdges[i].get(j)].size() == 1) {
+    					if(availEdges[i].size() > 1) {
+    						
+    						continue;
+    					}
+    					else {
+    						
+    						ePath.add(availEdges[i].get(j));
+    						euler += availEdges[i].get(j);
+    						nextVertix = availEdges[i].get(j);
+    						availEdges[i].remove(Integer.valueOf(j));
+    						availEdges[nextVertix].remove(i);
+    						counter++;
+    						break;
+    					}
+    				}
+    				else {
+    					ePath.add(availEdges[i].get(j));
+    					euler += availEdges[i].get(j);
+    					nextVertix = availEdges[i].get(j);
+    					availEdges[nextVertix].remove(i);
+    					availEdges[i].remove(j);
+    					counter++;
+    					break;
+    				}
+    			}
+    			i = nextVertix;
+    		}
+    		//ePath.add(0);
+    	}
+    	else if(condition == -1) {
+    		ePath.add(-1);
+    		euler += "-1";
+    	}
+    	else {
+    		i = condition;
+    		ePath.add(condition);
+    		euler += condition;
+    		while(counter < E) {
+    			
     			for(int j = 0; j < availEdges[i].size(); j++) {
     				if(availEdges[availEdges[i].get(j)].size() == 1) {
     					if(availEdges[i].size() > 1) {
@@ -119,15 +184,21 @@ public class mGraph {
     					}
     					else {
     						ePath.add(availEdges[i].get(j));
+    						euler += availEdges[i].get(j);
     						nextVertix = availEdges[i].get(j);
-    						availEdges[i].remove(j);
+    						availEdges[i].remove(Integer.valueOf(j));
+    						availEdges[nextVertix].remove(i);
+    						counter++;
     						break;
     					}
     				}
     				else {
     					ePath.add(availEdges[i].get(j));
+    					euler += availEdges[i].get(j);
     					nextVertix = availEdges[i].get(j);
+    					availEdges[nextVertix].remove(i);
     					availEdges[i].remove(j);
+    					counter++;
     					break;
     				}
     			}
